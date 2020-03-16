1030388155933587f15397e53bbfdd03f475a7e4
phlpmwa <mwaphiwa@gmail.com>
2020-03-15 23:29:18 -0700

    show single record

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -21,7 +21,8 @@ class PizzaController extends Controller
 
     public function show($id)
     {
-        return view('pizzas.show',['id'=>$id]);
+        $pizza=Pizza::findorfail($id);
+        return view('pizzas.show',['pizza'=>$pizza]);
     }
 
     public function create()
