d23dd919dbc462f1a8f813902b6866955731c484
phlpmwa <mwaphiwa@gmail.com>
2020-03-16 00:22:00 -0700

    creating webform

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,13 +1,24 @@
   @extends('layouts.layout')
   @section('content')
        
-       <div class="flex-center position-ref full-height">
-            
-            
-            <div class="content">
-                <div class="title m-b-md">
-                   Add a new Pizza
-                </div>
-                            </div>
+       
+        <div class="wrapper create-pizza">
+<h1>Create a New Pizza</h1>
+<form action="/pizzas" method="POST"></form>
+<label for="Name">Your Name:</label>
+<input type="text" id="name" name="name">
+<label for="Type">Choose Pizza Type:</label>
+<select name="type" id="type">
+    <option value="margarita">Margarita</option>
+    <option value="hawaiian">Hawaiian</option>
+    <option value="veg suprem">Veg Supreme</option>
+</select>
+<label for="base">Choose base Type:</label>
+<select name="base" id="base">
+    <option value="cheesy crust">cheesy crust</option>
+    <option value="garlic crust">garlic crust</option>
+    <option value="thin $ cripsy">VThin and cripsy</option>
+</select>
+<input type="submit" value="Order Pizza">
         </div>
     @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
                 <div class="title m-b-md">
                    The North's Best Pizza
                 </div>
-
+<a href="/pizzas/create">Order a Pizza</a>
                             </div>
         </div>
 @endsection
